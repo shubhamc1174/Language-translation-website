8561d31e0a03bd32adc9044f4ba9907390bf2338
shubhamc1174 <107864038+shubhamc1174@users.noreply.github.com>
2022-10-03 21:50:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+
+
+def index():
+    import requests
+
+
+    response = requests.request()
+
+    print(response.text)
+
+
+index()
